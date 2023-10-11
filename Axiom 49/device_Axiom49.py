66ff4d28663a2e43853cfd53c59f8bf9ebb56cd1
Michael Mortensen <mvmorten@live.com>
2023-10-11 16:59:39 -0700

    stop, play and record transport controls working

---
+++ Axiom 49/device_Axiom49.py
@@ -0,0 +1,27 @@
+# name=Axiom 49
+
+import transport
+
+rewind_button = 0x15
+fastforward_button = 0x16
+stop_button = 0x17
+play_button = 0x18
+record_button = 0x19
+
+def OnMidiMsg(event):
+	event.handled = False
+	if event.data2 > 0:	
+		if event.data1 == stop_button:
+			transport.stop()
+			event.handled = True
+		if event.data1 == rewind_button:
+			event.handled = True
+		if event.data1 == fastforward_button:
+			event.handled = True
+		if event.data1 == play_button:
+			transport.start()
+			event.handled = True
+		if event.data1 == record_button:
+			transport.record()
+			event.handled = True            
+
