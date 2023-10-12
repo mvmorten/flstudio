bef186ab7f9386d67122748af82d1255d0a1175b
Michael Mortensen <mvmorten@live.com>
2023-10-11 17:58:35 -0700

    add loop recording toggle

--- Axiom 49/device_Axiom49.py
+++ Axiom 49/device_Axiom49.py
@@ -1,7 +1,19 @@
 # name=Axiom 49
 
+import arrangement
+import channels
+import device
+import general
+import midi
+import mixer
+import patterns
+import plugins
 import transport
+import ui
 
+loop_button = 0x14
+rewind_button = 0x15
+fastforward_button = 0x16
 stop_button = 0x17
 play_button = 0x18
 record_button = 0x19
@@ -9,6 +21,9 @@ record_button = 0x19
 def OnMidiMsg(event):
 	event.handled = False
 	if event.data2 > 0:	
+		if event.data1 == 0x14:		 
+			transport.globalTransport(113, 1)
+			event.handled = True
 		if event.data1 == stop_button:
 			transport.stop()
 			event.handled = True
